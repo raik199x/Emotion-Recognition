d8b69b6002b799f3d986ac4af7b60a6515196a8c
Alexander <raik199x@mail.ru>
2024-02-26 11:17:13 +0300

    Added activatino function, temporarly ignored model push to git, model is learning

--- DeepLearning/model.py
+++ DeepLearning/model.py
@@ -1,14 +1,13 @@
 import torch
 import os
+import numpy as np
 from pathlib import Path
-from shared import pretrained_emotion_recognition_model, GetRelativePath
 from DeepLearning.settings import learning_rate, pytorch_device
 
 
 class EmotionClassificationModel(torch.nn.Module):
   def __init__(self):
     super().__init__()
-    self.backup_path = GetRelativePath(pretrained_emotion_recognition_model)
 
     # Creating layers
     self.input_layer = torch.nn.Linear(in_features=68 * 2, out_features=128)
@@ -16,6 +15,7 @@ class EmotionClassificationModel(torch.nn.Module):
     self.hidden_layer_2 = torch.nn.Linear(in_features=64, out_features=128)
     self.output_layer = torch.nn.Linear(in_features=128, out_features=7)  # 7 emotions
 
+    self.act = torch.nn.ReLU()
     self.loss_fn = torch.nn.CrossEntropyLoss()  # Multi class classification, includes nn.LogSoftmax and nn.NLLLoss
     self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)  # Most effective Adam and SGD
 
@@ -43,10 +43,16 @@ class EmotionClassificationModel(torch.nn.Module):
     # 5. Optimizer step
     self.optimizer.step()
 
-  def TestingEpoch(self, data, expected_result):
+  def TestingEpoch(self, data: np.array, expected_result: np.array):
     self.eval()
+    # 0. Converting function input into tensors
+    expected_tensor = torch.from_numpy(expected_result)
+    expected_tensor = expected_tensor.float().to(pytorch_device)
+    tensor = torch.from_numpy(data).to(pytorch_device)
+    tensor = tensor.float().flatten()
+
     with torch.inference_mode:
-      classification_result = self(data)
+      classification_result = self(tensor)
     return self.accuracy(expected_result, classification_result)
 
   def BackupModel(self, folder_path: str, file_name: str):
@@ -54,14 +60,17 @@ class EmotionClassificationModel(torch.nn.Module):
     MODEL_PATH.mkdir(parents=True, exist_ok=True)
     torch.save(self.state_dict(), os.path.join(folder_path, file_name))
 
-  def LoadModel(self, path_to_model):
+  def LoadModel(self, path_to_model: str):
     self.load_state_dict(torch.load(path_to_model))
     self.eval()
 
-  def accuracy(self, expected_result, model_result):
+  def accuracy(self, expected_result: torch.Tensor, model_result: torch.Tensor):
     correct = torch.eq(expected_result, model_result).sum().item()
     acc = (correct / len(model_result)) * 100
     return acc
 
-  def forward(self, x):  # must be redefined for any nn module
-    return self.output_layer(self.hidden_layer_2(self.hidden_layer_1(self.input_layer(x))))
+  def forward(self, x: torch.Tensor):  # must be redefined for any nn module
+    X = self.act(self.input_layer(x))
+    X = self.act(self.hidden_layer_1(X))
+    X = self.act(self.hidden_layer_2(X))
+    return self.output_layer(X)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from torch import __version__ as pytorch_version
+import torch
 from DeepLearning import dataset_parser, model, settings
 import os
 import numpy as np
@@ -9,16 +10,22 @@ if __name__ == "__main__":
   print(settings.pytorch_device)
 
   parser = dataset_parser.DatasetParser()
-  generator = parser.EmotionNpPointGenerator(parser.forLearning, parser.angry)
-  expect = np.array([1, 0, 0, 0, 0, 0, 0])
+  generator = parser.EmotionNpPointGenerator(parser.forLearning, parser.surprise)
+  expect = np.array([0, 0, 0, 0, 0, 0, 1])
+  values = next(generator)
+  values = next(generator)
   values = next(generator)
 
   emotion_recognition_model = model.EmotionClassificationModel().to(settings.pytorch_device)
   if os.path.exists(shared.pretrained_emotion_recognition_model):
+    print("reading backup")
     emotion_recognition_model.LoadModel(shared.pretrained_emotion_recognition_model)
-    print(emotion_recognition_model.state_dict())
+    # print(emotion_recognition_model.state_dict())
 
   emotion_recognition_model.TrainEpoch(values, expect)
 
   emotion_recognition_model.BackupModel(shared.data_folder, "emotion_recognition_model.pt")
-  print(emotion_recognition_model.state_dict())
+  # print(emotion_recognition_model.state_dict())
+  tensor = torch.from_numpy(values).to(settings.pytorch_device)
+  tensor = tensor.float().flatten()
+  print(emotion_recognition_model(tensor))
