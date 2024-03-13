0af85d394d04fb892c8cfe7ce6f54ab508dc2eac
Alexander <raik199x@mail.ru>
2024-03-13 14:46:53 +0300

    Device recognition fix

--- DeepLearning/settings.py
+++ DeepLearning/settings.py
@@ -1,5 +1,5 @@
 from torch.cuda import is_available as pytorch_is_cuda_working
 
 
-pytorch_device = "cuda" if pytorch_is_cuda_working else "cpu"
+pytorch_device = "cuda" if pytorch_is_cuda_working() else "cpu"
 learning_rate = 0.001
